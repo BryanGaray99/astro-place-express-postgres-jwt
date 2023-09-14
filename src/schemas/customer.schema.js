6452554fe140c3ae95ecf57a1911d9e21bbde6fa
BryanGaray <bryangarayacademico@gmail.com>
2023-09-13 21:12:14 -0500

    Customer schema fixed

--- src/schemas/customer.schema.js
+++ src/schemas/customer.schema.js
@@ -5,10 +5,6 @@ const name = Joi.string().min(1).max(50);
 const lastName = Joi.string().min(1).max(50);
 const phone = Joi.string();
 const userId = Joi.number().integer();
-const email = Joi.string().email();
-const password = Joi.string().min(8);
-const role = Joi.string().min(5);
-const nameUser = Joi.string().min(3).max(30);
 
 const createCustomerSchema = Joi.object({
   name: name.required(),
