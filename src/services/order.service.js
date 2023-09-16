ca914754a2d2979cf4a0e664c1a7847b10ef974d
BryanGaray <bryangarayacademico@gmail.com>
2023-09-15 21:29:27 -0500

    Profile endpoint

--- src/services/order.service.js
+++ src/services/order.service.js
@@ -37,6 +37,21 @@ class OrderService {
     return order;
   };
 
+  async findByUser(userId) {
+    const orders = await models.Order.findAll({
+      where: {
+        '$customer.user.id$': userId
+      },
+      include: [
+        {
+          association: 'customer',
+          include: ['user']
+        }
+      ]
+    });
+    return orders;
+  };
+
   async update(id, changes) {
     const order = await models.Order.findByPk(id);
     if (!order) {
